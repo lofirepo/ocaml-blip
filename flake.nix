698138b5e5edad07712e69980cc54a9b6d46f7fc
TG × ⊙ <*@tg-x.net>
2021-02-24 02:31:21 +0100

    nix flake: support multiple systems & ocaml version

--- flake.nix
+++ flake.nix
@@ -1,63 +1,113 @@
 {
   description = "BLIP: BLoom-then-flIP";
 
-  inputs = {
-    nixpkgs.url = github:NixOS/nixpkgs/master;
-  };
+  outputs = { self, nixpkgs }:
+    let
+      supportedSystems = [
+        "x86_64-linux" "aarch64-linux" "armv7l-linux"
+        "x86_64-darwin" "aarch64-darwin"
+      ];
+      supportedOcamlPackages = [
+        "ocamlPackages_4_10"
+        "ocamlPackages_4_11"
+        "ocamlPackages_4_12"
+      ];
+      defaultOcamlPackages = "ocamlPackages_4_11";
 
-  outputs = { self, nixpkgs }: {
+      forAllOcamlPackages = nixpkgs.lib.genAttrs supportedOcamlPackages;
+      forAllSystems = nixpkgs.lib.genAttrs supportedSystems;
+      nixpkgsFor =
+        forAllSystems (system:
+          import nixpkgs {
+            inherit system;
+            overlays = [ self.overlay ];
+          });
+    in
+      {
+        overlay = final: prev:
+          with final;
+          let mkOcamlPackages = prevOcamlPackages:
+                with prevOcamlPackages;
+                let ocamlPackages =
+                      {
+                        inherit ocaml;
+                        inherit findlib;
+                        inherit ocamlbuild;
+                        inherit opam-file-format;
+                        inherit buildDunePackage;
 
-    packages.x86_64-linux.ocaml-bitv =
-      with import nixpkgs { system = "x86_64-linux"; };
-      ocamlPackages.buildDunePackage rec {
-        pname = "bitv";
-        version = "1.4";
-        src = fetchFromGitHub {
-          owner = "backtracking";
-          repo = pname;
-          rev = version;
-          sha256 = "0mhnbwnw06s6kknxygm0fa51gb1wd6y8npfq6r5dvlmmpgf7xgm8";
-        };
-        useDune2 = true;
+                        bitv =
+                          buildDunePackage rec {
+                            pname = "bitv";
+                            version = "1.4";
+                            src = fetchFromGitHub {
+                              owner = "backtracking";
+                              repo = pname;
+                              rev = version;
+                              sha256 = "0mhnbwnw06s6kknxygm0fa51gb1wd6y8npfq6r5dvlmmpgf7xgm8";
+                            };
+
+                            useDune2 = true;
+                          };
+
+                        bloomf =
+                          buildDunePackage rec {
+                            pname = "bloomf";
+                            version = "0.1.0-bits";
+                            src = fetchFromGitHub {
+                              owner = "p2pcollab";
+                              repo = pname;
+                              rev = "cbffe83255cb12f5117825f8f8ebf363e18bd627";
+                              sha256 = "0p4l8fib72vmbyk5izlsfawyxfz3wgcg2c2vglqf5103y28xi1jg";
+                            };
+
+                            useDune2 = true;
+
+                            buildInputs = with ocamlPackages; [
+                              bitv
+                            ];
+                          };
+
+                        blip =
+                          buildDunePackage rec {
+                            pname = "blip";
+                            version = "0.0.1";
+                            src = self;
+
+                            useDune2 = true;
+                            doCheck = true;
+
+                            nativeBuildInputs = with ocamlPackages; [
+                              odoc
+                              ounit
+                            ];
+                            buildInputs = with ocamlPackages; [
+                              bloomf
+                              bitv
+                              nocrypto
+                            ];
+                          };
+                      };
+                in ocamlPackages;
+          in
+            let allOcamlPackages =
+                  forAllOcamlPackages (ocamlPackages:
+                    mkOcamlPackages ocaml-ng.${ocamlPackages}
+                  );
+            in
+              allOcamlPackages // {
+                ocamlPackages = allOcamlPackages.${defaultOcamlPackages};
+              };
+
+        packages =
+          forAllSystems (system:
+            forAllOcamlPackages (ocamlPackages:
+              nixpkgsFor.${system}.${ocamlPackages}
+            ));
+
+        defaultPackage =
+          forAllSystems (system:
+            nixpkgsFor.${system}.ocamlPackages.blip
+          );
       };
-
-    packages.x86_64-linux.ocaml-bloomf =
-      with import nixpkgs { system = "x86_64-linux"; };
-      ocamlPackages.buildDunePackage rec {
-        pname = "bloomf";
-        version = "0.2.0";
-        src = fetchFromGitHub {
-          owner = "mirage";
-          repo = pname;
-          rev = "v${version}";
-          sha256 = "0jq75sr4z059kzbz0lb4rdfbcx7pq0qs8wiww1pnhkhhpnmvw1aq";
-        };
-        useDune2 = true;
-
-        buildInputs = with pkgs.ocamlPackages; [
-          self.packages.x86_64-linux.ocaml-bitv
-        ];
-      };
-
-    packages.x86_64-linux.ocaml-blip =
-      with import nixpkgs { system = "x86_64-linux"; };
-      ocamlPackages.buildDunePackage rec {
-        pname = "blip";
-        version = "0.0.1";
-        src = self;
-        useDune2 = true;
-
-        buildInputs = with pkgs.ocamlPackages; [
-          self.packages.x86_64-linux.ocaml-bloomf
-          self.packages.x86_64-linux.ocaml-bitv
-          nocrypto
-        ];
-        nativeBuildInputs = with pkgs.ocamlPackages; [
-          odoc
-          ounit
-        ];
-      };
-
-    defaultPackage.x86_64-linux = self.packages.x86_64-linux.ocaml-blip;
-  };
 }
